1669180435d9e9f3e686e7949d835aeb9bcfbdef
Mikhail Grigoryan <grigoryan_m@mail.ru>
2019-05-30 00:07:51 +0300

    Transform input data.

--- Kaggle/HousePrices/src/datamaker.py
+++ Kaggle/HousePrices/src/datamaker.py
@@ -16,8 +16,16 @@ def unitary_coding(possible_val_lst, value):
         raise "Fatal error! Unrecognized symbol!"
     return coded_out
 
-all_attributes = ["Id", "MSSubClass", "MSZoning", "LotFrontage", "LotArea", "Street", "Alley"]
-number_attributes = ["LotFrontage", "LotArea"]
+all_attributes = \
+    [
+        ("MSSubClass", ["20", "30", "40", "45", "50", "60", "70", "75", "80", "85", "90", "120", "150", "160", "180", "190"]), #16
+        ("MSZoning", ["A", "C", "FV", "I", "RH", "RL", "RP", "RM"]), #8 = 24
+        ("LotFrontage", []), #2 = 26
+        ("LotArea", []), #2 = 28
+        ("Street", ["Grvl", "Pave"]), #2 = 30
+        ("Alley", ["Grvl", "Pave", "NA"]), #3 = 33
+        ("LotShape", ["Reg", "IR1", "IR2", "IR3"]) #4 = 37
+    ]
 
 def float_coding(valdct, attr, maxvals_dct):
     retarr = np.zeros(2)
@@ -33,40 +41,42 @@ def make_base_dictlst(file_name):
         reader = csv.DictReader(file_obj, delimiter=',')
         for line in reader:
             linedct = dict()
-            for attr in all_attributes:
-                linedct[attr] = line[attr]
+            for attr_descr in all_attributes:
+                attr_name = attr_descr[0]
+                linedct[attr_name] = line[attr_name]
             dctlst.append(linedct)
     return dctlst
 
 def make_max_dict(dctlst):
     maxvals_dct = dict()
-    for attrib in number_attributes:
-        max_val = 0
-        for valdct in dctlst:
-            strval = valdct[attrib]
-            if strval != "NA": #possible unknown value
-                fltval = float(strval)
-                if fltval < 0:
-                    raise "Unexpected negative number!"
-                max_val = max(max_val, fltval)
-        maxvals_dct[attrib] = max_val
+    for attr_descr in all_attributes:
+        attr_range = attr_descr[1]
+        if len(attr_range) == 0: #this is number attribute
+            attr_name = attr_descr[0]
+            max_val = 0
+            for valdct in dctlst:
+                strval = valdct[attr_name]
+                if strval != "NA": #possible unknown value
+                    fltval = float(strval)
+                    if fltval < 0:
+                        raise "Unexpected negative number!"
+                    max_val = max(max_val, fltval)
+            maxvals_dct[attr_name] = max_val
     return maxvals_dct
 
 def file_to_data(file_name):
-    dctlst = make_base_dictlst(file_name)
-    maxvals_dct = make_max_dict(dctlst)
+    dctlst = make_base_dictlst(file_name) #read file and make working dictionary
+    maxvals_dct = make_max_dict(dctlst) #make max values dictionary for number attributes
 
-    for valdct in dctlst:
-        inarr = unitary_coding(["20", "30", "40", "45", "50", "60", "70", "75", "80", "85", "90", "120", "150", "160", "180", "190"], valdct["MSSubClass"]) #16
-        inarr = np.hstack((inarr, unitary_coding(["A", "C", "FV", "I", "RH", "RL", "RP", "RM"], valdct["MSZoning"]))) #8 = 24
-        inarr = np.hstack((inarr, float_coding(valdct, "LotFrontage", maxvals_dct))) #2 = 26
-        inarr = np.hstack((inarr, float_coding(valdct, "LotArea", maxvals_dct)))  #2 = 28
-        inarr = np.hstack((inarr, unitary_coding(["Grvl", "Pave"], valdct["Street"])))  #2 = 30
-        inarr = np.hstack((inarr, unitary_coding(["Grvl", "Pave", "NA"], valdct["Alley"])))  #3 = 33
-
-        #inarr = np.hstack((inarr, [to_float(valdct["LotArea"]) / get_max_val("LotArea", dctlst)]))
-        #inarr = np.hstack((inarr, unitary_coding(["Grvl", "Pave"], valdct["Street"])))
-
+    for valdct in dctlst: #for each input example
+        inarr = np.ndarray(0)
+        for attribute in all_attributes:
+            attr_name = attribute[0]
+            attr_range = attribute[1]
+            if len(attr_range) == 0: #number attribute
+                inarr = np.hstack((inarr, float_coding(valdct, attr_name, maxvals_dct)))
+            else: #quality attribute
+                inarr = np.hstack((inarr, unitary_coding(attr_range, valdct[attr_name])))
         print(len(inarr))
         break
     return [0, 0]
